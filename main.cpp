57ccda42700074486e5996ba5b2d9e16216b17e8
AnastasiyaNovikovaa <43859713+AnastasiyaNovikovaa@users.noreply.github.com>
2019-05-10 15:37:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,42 +8,58 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	setlocale(LC_ALL, "rus");
-	cout << "Добрый день!\n";
-	cout << "Лабораторная работа по теме. Алгоритм Флойда-Уоршелла\n";
-	cout << "Работу выполнила Новикова Анастасия, гр.7302\n";
+	cout << "Р”РѕР±СЂС‹Р№ РґРµРЅСЊ!\n";
+	cout << "Р›Р°Р±РѕСЂР°С‚РѕСЂРЅР°СЏ СЂР°Р±РѕС‚Р° РїРѕ С‚РµРјРµ. РђР»РіРѕСЂРёС‚Рј Р¤Р»РѕР№РґР°-РЈРѕСЂС€РµР»Р»Р°\n";
+	cout << "Р Р°Р±РѕС‚Сѓ РІС‹РїРѕР»РЅРёР»Р° РќРѕРІРёРєРѕРІР° РђРЅР°СЃС‚Р°СЃРёСЏ, РіСЂ.7302\n";
 	cout << endl;
 	vector<string> strings_for_reading;
-	auto* cities = new List<string>;  //вершины графа
-	unsigned short** matrix_all_itinerary;  // матрица всех путей
-	unsigned short** min_itenerary;  //матрица минимальных путей
+	auto* cities = new List<string>;  //РІРµСЂС€РёРЅС‹ РіСЂР°С„Р°
+	unsigned short** matrix_all_itinerary;  // РјР°С‚СЂРёС†Р° РІСЃРµС… РїСѓС‚РµР№
+	unsigned short** min_itenerary;  //РјР°С‚СЂРёС†Р° РјРёРЅРёРјР°Р»СЊРЅС‹С… РїСѓС‚РµР№
 
-	strings_for_reading = read_file("Input.txt"); //считываем данные из файла
+	strings_for_reading = read_file("Input.txt"); //СЃС‡РёС‚С‹РІР°РµРј РґР°РЅРЅС‹Рµ РёР· С„Р°Р№Р»Р°
 
 	for (int i = 0; i < strings_for_reading.size(); i++) {
-		getting_cities(strings_for_reading[i], cities);  //Получение всех городов
+		getting_cities(strings_for_reading[i], cities);  //РџРѕР»СѓС‡РµРЅРёРµ РІСЃРµС… РіРѕСЂРѕРґРѕРІ
 	}
 	
-	cout << endl << "Список всех городов: ";
+	cout << endl << "РЎРїРёСЃРѕРє РІСЃРµС… РіРѕСЂРѕРґРѕРІ: ";
 	cout << cities;
 	size_t  num = cities->get_size();
-	cout << "Количество считанных городов:" << num << "\n";
+	cout << "РљРѕР»РёС‡РµСЃС‚РІРѕ СЃС‡РёС‚Р°РЅРЅС‹С… РіРѕСЂРѕРґРѕРІ:" << num << "\n";
 	matrix_all_itinerary = new unsigned short* [cities->get_size()]; 
 	
-	make_matrix_bases(matrix_all_itinerary, cities->get_size());//матрица всех путей
+	make_matrix_bases(matrix_all_itinerary, cities->get_size());//РјР°С‚СЂРёС†Р° РІСЃРµС… РїСѓС‚РµР№
 
 	for (int i = 0; i < strings_for_reading.size(); i++) {
 		make_matrix(strings_for_reading[i], cities, matrix_all_itinerary);
 	}
 
-	cout << endl << "Таблица всех маршрутов с ценами на перелет " << endl;
+	cout << endl << "РўР°Р±Р»РёС†Р° РІСЃРµС… РјР°СЂС€СЂСѓС‚РѕРІ СЃ С†РµРЅР°РјРё РЅР° РїРµСЂРµР»РµС‚ " << endl;
 	print_matrix(cities->get_size(), matrix_all_itinerary);
 	cout << endl;
+
+	
 	cout << "********************************************************************************\n";
 
 	min_itenerary = find_min_paths(cities->get_size(), matrix_all_itinerary);
-	cout << endl << "Таблица маршрутов между городами с указанием наименьшей стоимости: " << endl;
+	cout << endl << "РўР°Р±Р»РёС†Р° РјР°СЂС€СЂСѓС‚РѕРІ РјРµР¶РґСѓ РіРѕСЂРѕРґР°РјРё СЃ СѓРєР°Р·Р°РЅРёРµРј РЅР°РёРјРµРЅСЊС€РµР№ СЃС‚РѕРёРјРѕСЃС‚Рё: " << endl;
 	print_matrix(cities->get_size(), min_itenerary);
-	cout << "Спасибо за внимание! До свидания!\n";
+	cout << endl;
+	string city1;
+	string city2;
+	cout << "Р’РІРµРґРёС‚Рµ РїРµСЂРІС‹Р№ РіРѕСЂРѕРґ ";
+	cin >> city1;
+	cout << endl;
+	cout << "Р’РІРµРґРёС‚Рµ РІС‚РѕСЂРѕР№ РіРѕСЂРѕРґ ";
+	cin >> city2;
+	cout << endl;
+
+	cout << "РљСЂР°С‚С‡Р°Р№С€РµРµ СЂР°СЃСЃС‚РѕСЏРЅРёРµ РјРµР¶РґСѓ РіРѕСЂРѕРґРѕРј " << city1 << " Рё РіРѕСЂРѕРґРѕРј " << city2 << "=";
+	unsigned short minimal_way_ = minimal_way(city1, city2,  min_itenerary, cities);
+	std::cout << minimal_way_;
+	cout << endl;
+	cout << "РЎРїР°СЃРёР±Рѕ Р·Р° РІРЅРёРјР°РЅРёРµ! Р”Рѕ СЃРІРёРґР°РЅРёСЏ!\n";
 	cout << "********************************************************************************\n";
 	
 }
